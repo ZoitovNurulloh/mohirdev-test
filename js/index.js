901b55fd72f70dc4dea7340187eb81d4eca8256b
ZoitovNurillo <nurillozoitov97@gmail.com>
2023-01-22 11:32:43 +0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// const name = "Nurullo";
+// console.log(name + " ismida " + name.length + " belgi bor");
+
+// const  lastName = `Zoidov`;
+// const firstName = `Nurullo`;
+// const age = 21;
+// console.log(`Mening ismim ${lastName} va unda ${lastName.length} ta belgi bor`);
+
